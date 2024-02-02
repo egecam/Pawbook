baccdfc8fd999ffa4f58d54e464c978c77a7c413
egecam <egecam000@gmail.com>
2024-02-02 15:15:04 +0300

    Initial Commit

---
+++ Pawbook/PawbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawbookApp.swift
+//  Pawbook
+//
+//  Created by Ege Çam on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PawbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
