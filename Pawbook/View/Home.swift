b14cd1e9414310c98f68732fe092916a4f297c0a
ege <egecam000@gmail.com>
2024-02-06 02:47:52 +0300

    localization file

--- Pawbook/View/Home.swift
+++ Pawbook/View/Home.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import MapKit
 
-struct ContentView: View {
+struct Home: View {
     @StateObject var catViewModel1 = CatViewModel(catModel: CatModel(name: "Arwen", breed: "Tabby", age: 1.0, neighborhood: "Cihangir", coordinate: CLLocationCoordinate2D(latitude: 41.03252586801304, longitude: 28.984382222365696), thumbnailImage: "https://cdn2.thecatapi.com/images/dj5.jpg"))
     @StateObject var catViewModel2 = CatViewModel(catModel: CatModel(name: "Zeus", breed: "Tabby", age: 13.0, neighborhood: "Altayçeşme", coordinate: CLLocationCoordinate2D(latitude: 40.936933012654116, longitude: 29.131188164737058), thumbnailImage: "https://cdn2.thecatapi.com/images/RqIGMF79u.jpg"))
     @StateObject var catViewModel3 = CatViewModel(catModel: CatModel(name: "Simba", breed: "Siamese", age: 4.0, neighborhood: "Moda", coordinate: CLLocationCoordinate2D(latitude: 40.98185491474414, longitude: 29.023143536835203), thumbnailImage: "https://cdn2.thecatapi.com/images/MTU0NzEwOA.jpg"))
@@ -63,5 +63,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    Home()
 }
---
+++ Pawbook/ViewModel/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  Pawbook
+//
+//  Created by Ege Çam on 5.02.2024.
+//
+
+import Foundation
