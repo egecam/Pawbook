7c2f865b4f20c95d48034060df4aaab5d14240ef
ege <egecam000@gmail.com>
2024-02-08 01:06:49 +0300

    project configurations

---
+++ Pawbook/View/ThumbnailImage.swift
@@ -0,0 +1,8 @@
+//
+//  ThumbnailImage.swift
+//  Pawbook
+//
+//  Created by Ege Çam on 7.02.2024.
+//
+
+import Foundation
---
+++ PreviewSampleData.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewSampleData.swift
+//  Pawbook
+//
+//  Created by Ege Çam on 8.02.2024.
+//
+
+import Foundation
