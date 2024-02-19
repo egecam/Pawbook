31089dd8436dc6191bf1de5cfced9f60bf816b68
ege <egecam000@gmail.com>
2024-02-19 14:51:35 +0300

    create User model

--- Pawbook/Model/User.swift
+++ Pawbook/Model/User.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class User {
+    var id = UUID()
+    var username: String
+    var email: String
+    var password: String
+    var bio: String
+    var profilePicture: String
+    var registeredCats: [Cat]?
+    
+    init(id: UUID = UUID(), username: String, email: String, password: String, bio: String, profilePicture: String, registeredCats: [Cat]? = nil) {
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+        self.bio = bio
+        self.profilePicture = profilePicture
+        self.registeredCats = registeredCats
+    }
+}
